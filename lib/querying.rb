cf7e11071ab35b3e3a8e4b03cb7557277d8e4dfd
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-15 13:11:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT * FROM books.titles, books.years
+  "SELECT * books.titles, books.years
    FROM series
    JOIN books
    ON series.id = books.series_id
